97dd931e9afda770e252af8ca86cfa25bd56ce38
Ammar Askar <ammar@ammaraskar.com>
2020-11-09 23:48:05 -0800

    Add PoC for libsbc

---
+++ poc/0027-libsbc.rs
@@ -0,0 +1,35 @@
+/*!
+```crux-poc
+[target]
+crate = "libsbc"
+version = "0.1.4"
+
+[[target.peer]]
+crate = "static_assertions"
+version = "1.1.0"
+
+[test]
+analyzers = ["SendSyncChecker"]
+
+[report]
+title = "libsbc lacks a Send constraint on its Send trait"
+description = """
+"""
+code_snippets = ["https://github.com/mvertescher/libsbc-rs/blob/7278b23901f93d956d9739fdfc4ced147cc3f242/src/lib.rs#L34-L38"]
+patched = []
+informational = "unsound"
+issue_url = "https://github.com/mvertescher/libsbc-rs/issues/4"
+issue_date = "2020-11-10"
+```
+!*/
+#![forbid(unsafe_code)]
+
+use libsbc::Decoder;
+
+use std::io;
+use static_assertions::{assert_not_impl_all, assert_impl_all};
+
+fn main() {
+    assert_not_impl_all!(io::StdinLock : Send);
+    assert_not_impl_all!(Decoder<io::StdinLock> : Send);
+}
