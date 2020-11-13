0ec38c8c116cf5fc0a3b3ed49dfdf7459fca1496
ykim837 <ykim837@gatech.edu>
2020-11-13 17:28:19 -0500

    write poc0037-concread & report to GitHub

---
+++ poc/0037-concread.rs
@@ -0,0 +1,57 @@
+/*!
+```crux-poc
+[target]
+crate = "concread"
+version = "0.2.5"
+
+[test]
+analyzers = ["SendSyncChecker"]
+
+[report]
+title = ""
+description = """
+issue description"""
+code_snippets = [https://github.com/kanidm/concread/blob/08b258f4dbe7228ac7236f5b0f9d1a5f3c64f14f/src/arcache/mod.rs#L168]
+patched = []
+informational = "unsound"
+issue_url = "https://github.com/kanidm/concread/issues/48"
+issue_date = 2020-10-28
+```
+!*/
+#![forbid(unsafe_code)]
+use concread::arcache::ARCache;
+
+use std::sync::Arc;
+use std::rc::Rc;
+
+fn main() {
+    let non_sync_item = Rc::new(0); // neither `Send` nor `Sync`
+    assert_eq!(Rc::strong_count(&non_sync_item), 1);
+
+    let cache = ARCache::<i32, Rc<u64>>::new_size(5, 5);
+    let mut writer = cache.write();
+    writer.insert(0, non_sync_item);
+    writer.commit();
+
+    let arc_parent = Arc::new(cache);
+
+    let mut handles = vec![];
+    for _ in 0..5 {
+        let arc_child = arc_parent.clone();
+        let child_handle = std::thread::spawn(move || {
+            let reader = arc_child.read(); // new Reader of ARCache
+            let smuggled_rc = reader.get(&0).unwrap();
+            
+            for _ in 0..1000 {
+                let _dummy_clone = Rc::clone(&smuggled_rc); // Increment `strong_count` of `Rc`
+                // When `_dummy_clone` is dropped, `strong_count` is decremented.
+            }
+        });
+        handles.push(child_handle);
+    }
+    for handle in handles {
+        handle.join().expect("failed to join child thread");
+    }
+
+    assert_eq!(Rc::strong_count(arc_parent.read().get(&0).unwrap()), 1);
+}
