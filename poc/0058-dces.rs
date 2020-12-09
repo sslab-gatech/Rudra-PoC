e40fcfd71003a50f8d0329702499fb7104ce9098
Ammar Askar <ammar@ammaraskar.com>
2020-12-09 08:00:56 -0800

    Add PoC for dces

---
+++ poc/0058-dces.rs
@@ -0,0 +1,52 @@
+/*!
+```rudra-poc
+[target]
+crate = "dces"
+version = "0.2.0"
+
+[test]
+analyzers = ["SendSyncChecker"]
+
+[report]
+issue_date = 2020-12-09
+issue_url = "https://gitlab.redox-os.org/redox-os/dces-rust/-/issues/8"
+```
+!*/
+#![forbid(unsafe_code)]
+
+use dces::prelude::*;
+use dces::entity::EntityStore;
+
+use std::rc::Rc;
+
+struct MyEntityStore {
+    entity_store_rc: Rc<i32>
+}
+
+impl EntityStore for MyEntityStore {
+    fn register_entity(&mut self, entity: impl Into<Entity>) {
+        println!("Thread: {:p}", self.entity_store_rc);
+        // Races with the Rc in the main thread.
+        for _ in 0..100_000_000 {
+            self.entity_store_rc.clone();
+        }
+    }
+
+    fn remove_entity(&mut self, entity: impl Into<Entity>) { }
+}
+
+fn main() {
+    let rc = Rc::new(42);
+
+    let entity_store = MyEntityStore { entity_store_rc: rc.clone() };
+    let mut world = World::from_stores(entity_store, ComponentStore::default());
+
+    std::thread::spawn(move || {
+        world.create_entity().build();
+    });
+
+    println!("Main:   {:p}", rc);
+    for _ in 0..100_000_000 {
+        rc.clone();
+    }
+}
