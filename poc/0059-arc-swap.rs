b3ffa499ad4b910775c0db92e93be3c3cd0987d2
Yechan Bae <yechan@gatech.edu>
2020-12-09 22:13:26 -0500

    Report 0059-arc-swap

---
+++ poc/0059-arc-swap.rs
@@ -0,0 +1,64 @@
+/*!
+```rudra-poc
+[target]
+crate = "arc-swap"
+version = "1.0.0"
+
+[test]
+analyzers = ["manual", "SendSyncChecker"]
+
+[report]
+issue_url = "https://api.github.com/repos/vorner/arc-swap/issues/45"
+issue_date = 2020-12-09
+```
+!*/
+#![forbid(unsafe_code)]
+
+use arc_swap::access::Map;
+use arc_swap::access::{Access, Constant};
+
+static CORRECT_ADDR: &str = "I'm pointing to the correct location!";
+
+#[derive(Clone)]
+struct MemoryChecker {
+    // should be always CORRECT_ADDR
+    message: &'static str,
+}
+
+impl MemoryChecker {
+    pub fn new() -> Self {
+        MemoryChecker {
+            message: CORRECT_ADDR,
+        }
+    }
+
+    pub fn validate(&self) {
+        println!(
+            "Pointing to {:?}, len {}",
+            self.message as *const _,
+            self.message.len()
+        );
+        println!("Message: {}", self.message);
+    }
+}
+
+fn overwrite() {
+    let a = 123;
+    let b = 456;
+    println!("Overwriting stack content {} {}", a, b);
+}
+
+fn main() {
+    let constant = Constant(MemoryChecker::new());
+    constant.0.validate();
+
+    // Create a map with identity mapping
+    let map = Map::new(constant, |checker: &MemoryChecker| checker);
+
+    // After calling this, `value` pointer of `MapGuard` points to a dangling stack address
+    let loaded = map.load();
+
+    overwrite();
+
+    loaded.validate();
+}
