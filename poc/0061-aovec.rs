93f50bc7da1986a0b0f2e5b506e6c85b8ac4cec2
Ammar Askar <ammar@ammaraskar.com>
2020-12-09 23:21:40 -0800

    Add aovec PoC

---
+++ poc/0061-aovec.rs
@@ -0,0 +1,37 @@
+/*!
+```rudra-poc
+[target]
+crate = "aovec"
+version = "1.1.0"
+
+[test]
+analyzers = ["SendSyncChecker"]
+
+[report]
+issue_date = 2020-12-10
+rustsec_url = "https://github.com/RustSec/advisory-db/pull/528"
+```
+!*/
+#![forbid(unsafe_code)]
+
+use aovec::Aovec;
+use std::rc::Rc;
+
+fn main() {
+    let vec = Aovec::new(1);
+
+    let rc = Rc::new(42);
+    vec.push(rc.clone());
+
+    std::thread::spawn(move || {
+        println!("Thread: {:p}", vec[0]);
+        for _ in 0..100_000_000 {
+            vec[0].clone();
+        }
+    });
+
+    println!("Main:   {:p}", rc);
+    for _ in 0..100_000_000 {
+        rc.clone();
+    }
+}
