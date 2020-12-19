cf0f742981af0e035d264ff75ea705074de362d7
Yechan Bae <yechan@gatech.edu>
2020-12-18 20:52:37 -0500

    Report 0069-rusb

---
+++ poc/0069-rusb.rs
@@ -0,0 +1,19 @@
+/*!
+```rudra-poc
+[target]
+crate = "rusb"
+version = "0.6.5"
+
+[test]
+analyzers = ["SendSyncChecker"]
+
+[report]
+issue_url = "https://github.com/a1ien/rusb/issues/44"
+issue_date = 2020-12-18
+```
+!*/
+#![forbid(unsafe_code)]
+
+fn main() {
+    panic!("This issue was reported without PoC");
+}
