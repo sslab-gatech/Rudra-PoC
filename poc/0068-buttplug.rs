2fecaa0bbd1135176f1df7035791a18cd47d40a9
ykim837 <ykim837@gatech.edu>
2020-12-18 20:39:09 -0500

    report poc-0068-buttplug to maintainer

---
+++ poc/0068-buttplug.rs
@@ -0,0 +1,63 @@
+/*!
+```rudra-poc
+[target]
+crate = "buttplug"
+version = "0.10.0"
+
+[[target.peer]]
+crate = "futures"
+version = "0.3.8"
+
+[test]
+analyzers = ["SendSyncChecker"]
+
+[report]
+issue_url = "https://github.com/buttplugio/buttplug-rs/issues/225"
+issue_date = 2020-12-18
+```
+!*/
+#![forbid(unsafe_code)]
+use buttplug::util::future::ButtplugFuture;
+use futures::executor;
+
+use std::cell::Cell;
+use std::sync::Arc;
+use std::thread;
+
+#[derive(Debug, Clone, Copy)]
+enum RefOrInt<'a> {
+    Ref(&'a u64),
+    Int(u64),
+}
+static X: u64 = 0;
+
+fn main() {
+    let future = ButtplugFuture::default();    
+    let shared = future.get_state_clone();
+
+    thread::spawn(move || {
+        let shared = shared;
+
+        let cell = Arc::new(Cell::new(RefOrInt::Int(0xdeadbeef)));
+        shared.set_reply(Arc::clone(&cell));
+
+        loop {
+            cell.set(RefOrInt::Int(0xdeadbeef));
+            cell.set(RefOrInt::Ref(&X))
+        }
+    });
+
+    let smuggled_cell: Arc<Cell<RefOrInt>> = executor::block_on(future);
+    println!("Future resolved");
+
+    loop {
+        if let RefOrInt::Ref(addr) = smuggled_cell.get() {
+            if addr as *const _ as usize != 0xdeadbeef {
+                continue;
+            }
+            // Due to the data race, obtaining Ref(0xdeadbeef) is possible
+            println!("Pointer is now: {:p}", addr);
+            println!("Dereferencing addr will now segfault: {}", *addr);
+        }
+    }
+}
