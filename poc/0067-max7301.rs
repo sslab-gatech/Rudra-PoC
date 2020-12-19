5bba4ad88b4d49d8506d39876921f3263f27fcdc
Yechan Bae <yechan@gatech.edu>
2020-12-18 19:06:03 -0500

    Report 0067-max7301

---
+++ poc/0067-max7301.rs
@@ -0,0 +1,19 @@
+/*!
+```rudra-poc
+[target]
+crate = "max7301"
+version = "0.1.0"
+
+[test]
+analyzers = ["SendSyncChecker"]
+
+[report]
+issue_url = "https://github.com/edarc/max7301/issues/1"
+issue_date = 2020-12-18
+```
+!*/
+#![forbid(unsafe_code)]
+
+fn main() {
+    panic!("This issue was reported without PoC");
+}
