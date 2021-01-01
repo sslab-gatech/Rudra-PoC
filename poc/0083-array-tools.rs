ce2f28593ca751de014a12a829f3da3b0b34592d
ykim837 <ykim837@gatech.edu>
2020-12-31 19:56:04 -0500

    report 0083-array-tools to maintainer

---
+++ poc/0083-array-tools.rs
@@ -0,0 +1,20 @@
+/*!
+```rudra-poc
+[target]
+crate = "array-tools"
+version = "0.2.10"
+
+[test]
+analyzers = ["PanicSafety"]
+cargo_toolchain = "nightly"
+
+[report]
+issue_url = "https://github.com/L117/array-tools/issues/2"
+issue_date = 2020-12-31
+```
+!*/
+#![forbid(unsafe_code)]
+
+fn main() {
+    panic!("This issue was reported without PoC");
+}
