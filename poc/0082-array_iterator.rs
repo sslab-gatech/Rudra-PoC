d06df8da41d20208b94262f0d4091994bb76cd12
ykim837 <ykim837@gatech.edu>
2020-12-31 19:15:00 -0500

    remove 'unsafe' in 0082-array_iterator.rs

--- poc/0082-array_iterator.rs
+++ poc/0082-array_iterator.rs
@@ -13,21 +13,18 @@ issue_date = 2020-12-31
 ```
 !*/
 #![forbid(unsafe_code)]
-use array_iterator::{Array, ArrayIterator};
-
 use std::mem::MaybeUninit;
 
-struct MyArr(Vec<String>);
+use array_iterator::{Array, ArrayIterator};
 
-// Slightly modified from
-// https://docs.rs/array_iterator/1.2.0/src/array_iterator/lib.rs.html#134-155
+struct MyArr(Vec<String>);
 impl Array<String> for MyArr {
     type MaybeUninit = Vec<MaybeUninit<String>>;
     
     fn into_maybeunint(self) -> Self::MaybeUninit {
         debug_assert_eq!(std::mem::size_of::<Self::MaybeUninit>(), std::mem::size_of::<Self>());
-        let mut partial: Self::MaybeUninit = unsafe { std::mem::transmute_copy(&self.0) };
-        std::mem::forget(self);
+
+        let mut partial: Self::MaybeUninit = self.0.into_iter().map(|x| MaybeUninit::new(x)).collect();
 
         // DANGEROUS! Appending uninitialized objects to iterator..
         for _ in 0..10 {
@@ -45,8 +42,6 @@ fn main() {
     ]);
     for x in ArrayIterator::new(my_arr) {
         println!("{} {:?}, {}", x.len(), x.as_bytes(), x);
-
-        // SEGFAULT within this loop
     }
 
     panic!(
