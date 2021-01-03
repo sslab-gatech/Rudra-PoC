0ab303653a7b0d9e85444fde670843014d29aeab
ykim837 <ykim837@gatech.edu>
2021-01-03 16:12:35 -0500

    report 0088-binjs_io to maintainer

---
+++ poc/0088-binjs_io.rs
@@ -0,0 +1,19 @@
+/*!
+```rudra-poc
+[target]
+crate = "binjs_io"
+version = "0.2.1"
+
+[test]
+analyzers = ["PanicSafety"]
+
+[report]
+issue_url = "https://github.com/binast/binjs-ref/issues/460"
+issue_date = 2021-01-03
+```
+!*/
+#![forbid(unsafe_code)]
+
+fn main() {
+    panic!("This issue was reported without PoC");
+}
