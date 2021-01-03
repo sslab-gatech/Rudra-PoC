beafb1a17cf997b604e640e4a1e44378990aed16
ykim837 <ykim837@gatech.edu>
2021-01-03 09:24:39 -0500

    0085-cdr & 0086-bra

---
+++ poc/0085-cdr.rs
@@ -0,0 +1,19 @@
+/*!
+```rudra-poc
+[target]
+crate = "cdr"
+version = "0.2.3"
+
+[test]
+analyzers = ["PanicSafety"]
+
+[report]
+issue_url = "https://github.com/hrektts/cdr-rs/issues/10"
+issue_date = 2021-01-02
+```
+!*/
+#![forbid(unsafe_code)]
+
+fn main() {
+    panic!("This issue was reported without PoC");
+}
