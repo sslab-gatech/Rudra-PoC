6239cb8d2427651fea6dad20fd8bb7ef312d5ceb
ykim837 <ykim837@gatech.edu>
2021-01-03 10:01:14 -0500

    report 0087-bronzedb-protocol to maintainer

---
+++ poc/0087-bronzedb-protocol.rs
@@ -0,0 +1,19 @@
+/*!
+```rudra-poc
+[target]
+crate = "bronzedb-protocol"
+version = "0.1.0"
+
+[test]
+analyzers = ["PanicSafety"]
+
+[report]
+issue_url = "https://github.com/Hexilee/BronzeDB/issues/1"
+issue_date = 2021-01-03
+```
+!*/
+#![forbid(unsafe_code)]
+
+fn main() {
+    panic!("This issue was reported without PoC");
+}
