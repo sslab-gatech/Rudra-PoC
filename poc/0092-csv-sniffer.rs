e265156a75488814f9fb2d078ef9e484c1d7fd6e
ykim837 <ykim837@gatech.edu>
2021-01-05 23:22:07 -0500

    0092-csv-sniffer

---
+++ poc/0092-csv-sniffer.rs
@@ -0,0 +1,19 @@
+/*!
+```rudra-poc
+[target]
+crate = "csv-sniffer"
+version = "0.1.1"
+
+[test]
+analyzers = ["PanicSafety"]
+
+[report]
+issue_url = "https://github.com/jblondin/csv-sniffer/issues/1"
+issue_date = 2021-01-05
+```
+!*/
+#![forbid(unsafe_code)]
+
+fn main() {
+    panic!("This issue was reported without PoC");
+}
