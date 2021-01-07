1cb01494301a52932c03bbe960e45d0882c9b96e
ykim837 <ykim837@gatech.edu>
2021-01-07 18:55:58 -0500

    report 0098-ash

---
+++ poc/0098-ash.rs
@@ -0,0 +1,19 @@
+/*!
+```rudra-poc
+[target]
+crate = "ash"
+version = "0.31.0"
+
+[test]
+analyzers = ["PanicSafety"]
+
+[report]
+issue_url = "https://github.com/MaikKlein/ash/issues/354"
+issue_date = 2021-01-07
+```
+!*/
+#![forbid(unsafe_code)]
+
+fn main() {
+    panic!("This issue was reported without PoC");
+}
