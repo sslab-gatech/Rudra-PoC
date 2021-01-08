b6d26e61e52d768dace26348dc8ec033d88b9f4a
ykim837 <ykim837@gatech.edu>
2021-01-07 22:55:18 -0500

    0102-columnar

---
+++ poc/0102-columnar.rs
@@ -0,0 +1,19 @@
+/*!
+```rudra-poc
+[target]
+crate = "columnar"
+version = "0.0.19"
+
+[test]
+analyzers = ["PanicSafety"]
+
+[report]
+issue_url = "https://github.com/frankmcsherry/columnar/issues/6"
+issue_date = 2021-01-07
+```
+!*/
+#![forbid(unsafe_code)]
+
+fn main() {
+    panic!("This issue was reported without PoC");
+}
