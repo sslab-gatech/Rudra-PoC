858f5bf44080163c2cd4744ccf19d3db5c8694d5
Yechan Bae <yechan@gatech.edu>
2021-01-08 00:30:31 -0500

    Report 0104-lexical-core

---
+++ poc/0104-lexical-core.rs
@@ -0,0 +1,19 @@
+/*!
+```rudra-poc
+[target]
+crate = "lexical-core"
+version = "0.7.4"
+
+[test]
+analyzers = ["manual", "PanicSafety"]
+
+[report]
+issue_url = "https://github.com/Alexhuszagh/rust-lexical/issues/53"
+issue_date = 2021-01-08
+```
+!*/
+#![forbid(unsafe_code)]
+
+fn main() {
+    panic!("This issue was reported without PoC");
+}
