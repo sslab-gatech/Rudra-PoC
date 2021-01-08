985f56b0a56548dbdfeb1ffde605174c2566f518
ykim837 <ykim837@gatech.edu>
2021-01-07 19:46:37 -0500

    report 0099-claxon

---
+++ poc/0099-claxon.rs
@@ -0,0 +1,19 @@
+/*!
+```rudra-poc
+[target]
+crate = "claxon"
+version = "0.4.3"
+
+[test]
+analyzers = ["PanicSafety"]
+
+[report]
+issue_url = "https://github.com/ruuda/claxon/issues/26"
+issue_date = 2021-01-07
+```
+!*/
+#![forbid(unsafe_code)]
+
+fn main() {
+    panic!("This issue was reported without PoC");
+}
