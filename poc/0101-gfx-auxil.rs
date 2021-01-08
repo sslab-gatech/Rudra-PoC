9a00cbb9c53e3dd09b57dde9ff2af69cd5579c17
ykim837 <ykim837@gatech.edu>
2021-01-07 22:34:46 -0500

    report 0101-gfx-auxil

---
+++ poc/0101-gfx-auxil.rs
@@ -0,0 +1,19 @@
+/*!
+```rudra-poc
+[target]
+crate = "gfx-auxil"
+version = "0.7.0"
+
+[test]
+analyzers = ["PanicSafety"]
+
+[report]
+issue_url = "https://github.com/gfx-rs/gfx/issues/3567"
+issue_date = 2021-01-07
+```
+!*/
+#![forbid(unsafe_code)]
+
+fn main() {
+    panic!("This issue was reported without PoC");
+}
