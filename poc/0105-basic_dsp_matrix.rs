1b2a030b18d0c8ffacf2fa5748671018dbd70984
ykim837 <ykim837@gatech.edu>
2021-01-10 21:17:19 -0500

    report 0105-basic_dsp_matrix

---
+++ poc/0105-basic_dsp_matrix.rs
@@ -0,0 +1,19 @@
+/*!
+```rudra-poc
+[target]
+crate = "basic_dsp_matrix"
+version = "0.9.0"
+
+[test]
+analyzers = ["PanicSafety"]
+
+[report]
+issue_url = "https://github.com/liebharc/basic_dsp/issues/47"
+issue_date = 2021-01-10
+```
+!*/
+#![forbid(unsafe_code)]
+
+fn main() {
+    panic!("This issue was reported without PoC");
+}
