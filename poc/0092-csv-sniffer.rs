454a8b181bfdd39e4ee462ed57bfe65feb9b9932
ykim837 <ykim837@gatech.edu>
2021-01-24 14:31:49 -0500

    RUSTSEC: 0092-csv-sniffer

--- poc/0092-csv-sniffer.rs
+++ poc/0092-csv-sniffer.rs
@@ -7,6 +7,7 @@ version = "0.1.1"
 [report]
 issue_url = "https://github.com/jblondin/csv-sniffer/issues/1"
 issue_date = 2021-01-05
+rustsec_url = "https://github.com/RustSec/advisory-db/pull/666"
 
 [[bugs]]
 analyzer = "UnsafeDataflow"
