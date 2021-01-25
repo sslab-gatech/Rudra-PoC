bd537c6447d97cc6b8dc98d926315297902a7681
ykim837 <ykim837@gatech.edu>
2021-01-24 22:11:00 -0500

    rustsec 0098-ash

--- poc/0098-ash.rs
+++ poc/0098-ash.rs
@@ -7,6 +7,7 @@ version = "0.31.0"
 [report]
 issue_url = "https://github.com/MaikKlein/ash/issues/354"
 issue_date = 2021-01-07
+rustsec_url = "https://github.com/RustSec/advisory-db/pull/680"
 
 [[bugs]]
 analyzer = "UnsafeDataflow"
