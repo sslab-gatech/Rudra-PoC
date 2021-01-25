efef1938def413e977f5f8cf02c20d85cc73af1a
ykim837 <ykim837@gatech.edu>
2021-01-25 10:45:58 -0500

    rustsec: 0031-abox

--- poc/0031-abox.rs
+++ poc/0031-abox.rs
@@ -11,6 +11,7 @@ version = "0.8.0"
 [report]
 issue_url = "https://github.com/SonicFrog/abox/issues/1"
 issue_date = 2020-11-10
+rustsec_url = "https://github.com/RustSec/advisory-db/pull/688"
 
 [[bugs]]
 analyzer = "SendSyncVariance"
