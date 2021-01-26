dcf4ed07d79c3e76ee39d4d1472b219128ce1b94
ykim837 <ykim837@gatech.edu>
2021-01-26 11:52:00 -0500

    rustsec 0044-signal-simple

--- poc/0044-signal-simple.rs
+++ poc/0044-signal-simple.rs
@@ -11,6 +11,7 @@ version = "0.8.0"
 [report]
 issue_url = "https://github.com/kitsuneninetails/signal-rust/issues/2"
 issue_date = 2020-11-15
+rustsec_url = "https://github.com/RustSec/advisory-db/pull/694"
 
 [[bugs]]
 analyzer = "SendSyncVariance"
