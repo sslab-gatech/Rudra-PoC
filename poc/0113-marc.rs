21fa47e6045ad1856d3c1f4ea5424361adbfd9fd
ykim837 <ykim837@gatech.edu>
2021-01-27 10:31:54 -0500

    rustsec 0113-marc

--- poc/0113-marc.rs
+++ poc/0113-marc.rs
@@ -7,6 +7,7 @@ version = "1.5.0"
 [report]
 issue_url = "https://github.com/blackbeam/rust-marc/issues/7"
 issue_date = 2021-01-26
+rustsec_url = "https://github.com/RustSec/advisory-db/pull/699"
 
 [[bugs]]
 analyzer = "UnsafeDataflow"
