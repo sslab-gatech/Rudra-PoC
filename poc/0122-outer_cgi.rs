54815e7084aa5527379428e16b6903063dea0d99
ykim837 <ykim837@gatech.edu>
2021-01-31 10:57:52 -0500

    report 122-outer_cgi

---
+++ poc/0122-outer_cgi.rs
@@ -0,0 +1,21 @@
+/*!
+```rudra-poc
+[target]
+crate = "outer_cgi"
+version = "0.2.0"
+
+[report]
+issue_url = "https://github.com/SolraBizna/outer_cgi/issues/1"
+issue_date = 2021-01-31
+
+[[bugs]]
+analyzer = "UnsafeDataflow"
+bug_class = "UninitExposure"
+bug_count = 2
+```
+!*/
+#![forbid(unsafe_code)]
+
+fn main() {
+    panic!("This issue was reported without PoC");
+}
