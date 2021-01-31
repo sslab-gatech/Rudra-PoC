37a211f2a81c0aaab5634b7bb7aa334dadbef0e5
ykim837 <ykim837@gatech.edu>
2021-01-30 20:18:46 -0500

    report 0119-quick-protobuf

---
+++ poc/0119-quick-protobuf.rs
@@ -0,0 +1,20 @@
+/*!
+```rudra-poc
+[target]
+crate = "quick-protobuf"
+version = "0.8.0"
+
+[report]
+issue_url = "https://github.com/tafia/quick-protobuf/issues/186"
+issue_date = 2021-01-30
+
+[[bugs]]
+analyzer = "UnsafeDataflow"
+bug_class = "PanicSafety"
+```
+!*/
+#![forbid(unsafe_code)]
+
+fn main() {
+    panic!("This issue was reported without PoC");
+}
