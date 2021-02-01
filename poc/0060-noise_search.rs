3cc1de7cbda4eb8b17447b731a53eba63534055f
ykim837 <ykim837@gatech.edu>
2021-01-31 23:25:00 -0500

    rustsec 0060-noise_search

--- poc/0060-noise_search.rs
+++ poc/0060-noise_search.rs
@@ -7,6 +7,7 @@ version = "0.7.0"
 [report]
 issue_url = "https://github.com/pipedown/noise/issues/72"
 issue_date = 2020-12-10
+rustsec_url = "https://github.com/RustSec/advisory-db/pull/731"
 
 [[bugs]]
 analyzer = "SendSyncVariance"
