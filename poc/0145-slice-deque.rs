650da74b545cf9c23d2d843139b27e3b9a1f37a1
Ammar Askar <ammar@ammaraskar.com>
2021-02-18 23:28:10 -0800

    Report slice_deque issue

---
+++ poc/0145-slice-deque.rs
@@ -0,0 +1,46 @@
+/*!
+```rudra-poc
+[target]
+crate = "slice-deque"
+version = "0.3.0"
+
+[report]
+issue_date = 2021-02-19
+issue_url = "https://github.com/gnzlbg/slice_deque/issues/90"
+
+[[bugs]]
+analyzer = "UnsafeDataflow"
+bug_class = "PanicSafety"
+rudra_report_locations = ["src/lib.rs:3016:5: 3040:6"]
+```
+!*/
+#![forbid(unsafe_code)]
+
+use slice_deque::SliceDeque;
+
+struct DropDetector(u32);
+
+impl Drop for DropDetector {
+    fn drop(&mut self) {
+        println!("Dropping {}", self.0);
+    }
+}
+
+fn main() {
+    let mut deq = SliceDeque::new();
+    deq.push_back(DropDetector(1));
+    deq.push_back(DropDetector(2));
+    deq.push_back(DropDetector(3));
+
+    let drained = deq
+        .drain_filter(|x| {
+            if x.0 == 1 {
+                true
+            } else if x.0 == 2 {
+                false
+            } else {
+                panic!("predicate panicked!");
+            }
+        })
+        .collect::<SliceDeque<_>>();;
+}
