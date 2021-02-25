27f3f9039fccd237409e893559cc3b6d6fa5e424
Ammar Askar <ammar@ammaraskar.com>
2021-02-25 13:55:50 -0800

    Report concread issue

---
+++ poc/0156-concread.rs
@@ -0,0 +1,46 @@
+/*!
+```rudra-poc
+[target]
+crate = "concread"
+version = "0.2.6"
+
+[report]
+issue_url = "https://github.com/kanidm/concread/issues/55"
+issue_date = 2021-02-25
+
+[[bugs]]
+analyzer = "UnsafeDataflow"
+bug_class = "PanicSafety"
+bug_count = 2
+rudra_report_locations = ["src/collections/bptree/node.rs:376:5: 543:6"]
+```
+!*/
+#![forbid(unsafe_code)]
+
+use concread::bptree::BptreeMap;
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+struct DropDetector(u32);
+
+impl Clone for DropDetector {
+    fn clone(&self) -> Self {
+        panic!("Panic on clone!");
+    }
+}
+
+impl Drop for DropDetector {
+    fn drop(&mut self) {
+        println!("Dropping {}", self.0);
+    }
+}
+
+fn main() {
+    let tree : BptreeMap<DropDetector, i32> = BptreeMap::new();
+    let mut writer = tree.write();
+
+    writer.insert(DropDetector(1), 1);
+    writer.commit();
+
+    let mut writer = tree.write();
+    writer.insert(DropDetector(0), 0);
+}
