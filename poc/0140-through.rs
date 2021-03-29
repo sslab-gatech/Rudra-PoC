8752d24854e130d8217ad8edca3199975ffc4e95
Ammar Askar <ammar@ammaraskar.com>
2021-03-29 13:12:28 -0700

    Report through to rustsec

--- poc/0140-through.rs
+++ poc/0140-through.rs
@@ -7,6 +7,7 @@ version = "0.1.0"
 [report]
 issue_url = "https://github.com/gretchenfrage/through/issues/1"
 issue_date = 2021-02-18
+rustsec_url = "https://github.com/RustSec/advisory-db/pull/850"
 
 [[bugs]]
 analyzer = "UnsafeDataflow"
