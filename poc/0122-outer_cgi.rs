4b81c6720bfb38c2dd2291608a0d8a914d44bd82
Ammar Askar <ammar@ammaraskar.com>
2021-04-05 08:34:37 -0700

    Fix rustsec id for outer_cgi

--- poc/0122-outer_cgi.rs
+++ poc/0122-outer_cgi.rs
@@ -8,7 +8,7 @@ version = "0.2.0"
 issue_url = "https://github.com/SolraBizna/outer_cgi/issues/1"
 issue_date = 2021-01-31
 rustsec_url = "https://github.com/RustSec/advisory-db/pull/864"
-rutsec_id = "RUSTSEC-2021-0051"
+rustsec_id = "RUSTSEC-2021-0051"
 
 [[bugs]]
 analyzer = "UnsafeDataflow"
