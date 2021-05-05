45669f56e85897945288e184411c9a596360c115
JOE1994 <joseph942010@gmail.com>
2021-05-05 01:22:29 -0400

    update log_analyzer to output stats for ranks

---
+++ paper/count_by_rank.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python3
+from common import *
+from collections import defaultdict
+from enum import IntEnum
+import glob
+import os
+import sys
+
+# When counting bugs per rank,
+# incorporate higher rank counts to lower rank counts
+class Rank(IntEnum):
+    HIGH = 2
+    MID = 1
+    LOW = 0
+
+HASHER = {
+    ## SendSyncVariance
+    "NaiveSendForSend": Rank.LOW,
+    "NaiveSyncForSync": Rank.LOW,
+    #
+    "ApiSyncforSync": Rank.MID,
+    "PhantomSendForSend": Rank.MID,
+    "RelaxSync": Rank.MID,
+    #
+    "ApiSendForSync": Rank.HIGH,
+    "RelaxSend": Rank.HIGH,
+    
+    ## UnsafeDataflow
+    "PtrAsRef": Rank.LOW,
+    "SliceFromRaw": Rank.LOW,
+    "SliceUnchecked": Rank.LOW,
+    "Transmute": Rank.LOW,
+    #
+    "CopyFlow": Rank.MID,
+    "ReadFlow": Rank.MID,
+    "WriteFlow": Rank.MID,
+    #
+    "VecFromRaw": Rank.HIGH,
+    "VecSetLen": Rank.HIGH,
+}
+
+def create_counter() -> dict:
+    cnt = {
+        "SendSyncVariance": {
+            Rank.HIGH: 0,
+            Rank.MID: 0,
+            Rank.LOW: 0,
+        },
+        "UnsafeDataflow": {
+            Rank.HIGH: 0,
+            Rank.MID: 0,
+            Rank.LOW: 0,
+        },
+    }
+    return cnt
+
+def count_reported_tp(
+    poc_detailed_metadata,
+    report_dir
+) -> dict:
+    assert (os.path.isdir(report_dir)), "invalid report directory"
+
+    # tp cnt
+    cnt = create_counter()
+
+    analyzers = ['SendSyncVariance', 'UnsafeDataflow']
+    for analyzer in analyzers:
+        for crate_id, metadata in poc_detailed_metadata[analyzer].items():
+            report_file_paths = list(glob.glob(f"{report_dir}/report-{crate_id}-*"))
+
+            assert len(report_file_paths) == 1 , f"{crate_id} {len(report_file_paths)}"
+
+            for report_file_path in report_file_paths:
+                with open(report_file_path, "r") as report_file:
+                    # match this with Rudra implementation
+                    report_file_str = report_file.read().replace("\t", "\\t").replace("\u001B", "\\u001B")
+                    report_dict = tomlkit.loads(report_file_str)
+
+                    # Used to get rid of duplicate reports for the same bug-type & span (macro deduplication)
+                    visited = dict()
+                    for report in filter(lambda x: x['analyzer'].startswith(analyzer), report_dict["reports"]):
+                        if report['location'] in metadata:
+                            analyzer_subcategories = report['analyzer'][len(analyzer) + 2:].split('/')
+                            for subcategory in analyzer_subcategories:
+                                key = report['location']
+                                if key not in visited or visited[key] < HASHER[subcategory]:
+                                    visited[key] = HASHER[subcategory]
+
+                    # Update TP counts
+                    for loc, highest_reached_rank in visited.items():
+                        for rank in Rank:
+                            if highest_reached_rank >= rank:
+                                cnt[analyzer][rank] += 1
+
+    return cnt
+
+def count_unreported_tp(report_dir):
+    assert (os.path.isdir(report_dir)), "invalid report directory"
+
+    cnt = create_counter()
+
+    unreported_metadata = get_unreported_metadata()
+
+    # UnsafeDataflow
+    for crate_id, metadata in unreported_metadata['UnsafeDataflow'].items():
+        report_file_paths = list(glob.glob(f"{report_dir}/report-{crate_id}-*"))
+        assert len(report_file_paths) >= 1, crate_id
+
+        for report_file_path in report_file_paths:
+            with open(report_file_path, "r") as report_file:
+                # match this with Rudra implementation
+                report_file_str = report_file.read().replace("\t", "\\t").replace("\u001B", "\\u001B")
+                report_dict = tomlkit.loads(report_file_str)
+
+                # Used to get rid of duplicate reports for the same bug-type & span (macro deduplication)
+                visited = dict()
+
+                for report in filter(lambda x: x['analyzer'].startswith('UnsafeDataflow'), report_dict['reports']):
+                    if report['location'] in metadata:
+                        analyzer_subcategories = report['analyzer'][16:].split('/')
+                        for subcategory in analyzer_subcategories:
+                            key = report['location']
+                            if key not in visited or visited[key] < HASHER[subcategory]:
+                                visited[key] = HASHER[subcategory]
+                # Update TP counts
+                for loc, highest_reached_rank in visited.items():
+                    for rank in Rank:
+                        if highest_reached_rank >= rank:
+                            cnt['UnsafeDataflow'][rank] += 1
+
+    # SendSyncVariance
+    for crate_id, metadata in unreported_metadata['SendSyncVariance'].items():
+        report_file_paths = list(glob.glob(f"{report_dir}/report-{crate_id}-*"))
+        assert len(report_file_paths) >= 1, crate_id
+
+        for report_file_path in report_file_paths:
+            with open(report_file_path, "r") as report_file:
+                # match this with Rudra implementation
+                report_file_str = report_file.read().replace("\t", "\\t").replace("\u001B", "\\u001B")
+                report_dict = tomlkit.loads(report_file_str)
+
+                # Get rid of duplicate reports for the same span (macro deduplication)
+                visited = dict()
+
+                for report in filter(lambda x: x['analyzer'].startswith('SendSyncVariance'), report_dict['reports']):
+                    if report['location'] in metadata:
+                        analyzer_subcategories = report['analyzer'][18:].split('/')
+                        for subcategory in analyzer_subcategories:
+                            key = report['location']
+                            if key not in visited or visited[key] < HASHER[subcategory]:
+                                visited[key] = HASHER[subcategory]
+                # Update TP counts
+                for loc, highest_reached_rank in visited.items():
+                    for rank in Rank:
+                        if highest_reached_rank >= rank:
+                            cnt['SendSyncVariance'][rank] += 1
+    return cnt
+
+
+if __name__ == "__main__":
+    detailed_poc_metadata = get_poc_detailed_metadata()
+
+    # Dictionary containing TruePositive counts for reported bugs
+    print('TP (Reported) Count... (by rank)')
+    tp_cnt = count_reported_tp(
+        detailed_poc_metadata,
+        f'/home/youngsuk/rudra-runner-home/campaign/{sys.argv[1]}/report'
+    )
+    print(tp_cnt)
+    
+    unreported_tp_cnt = count_unreported_tp(f'/home/youngsuk/rudra-runner-home/campaign/{sys.argv[1]}/report')
+    print('TP (Unreported) Count (by rank)')
+    print(unreported_tp_cnt)
--- paper/log_analyzer.py
+++ paper/log_analyzer.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
+import common
+import count_by_rank
+from count_by_rank import Rank
+
 import csv
 import datetime
 import glob
 import os
+import pandas as pd
 import sys
 import tomlkit
-from enum import Enum, auto
+from enum import IntEnum, Enum, auto
 
 # The order here should exactly match the actual analysis order of Rudra
 ANALYZERS = [
@@ -13,6 +18,42 @@ ANALYZERS = [
     "SendSyncVariance",
     "UnsafeDataflow",
 ]
+
+
+RANK_CATEGORIES = [
+    "UnsafeDataflow/0",
+    "UnsafeDataflow/1",
+    "UnsafeDataflow/2",
+    "SendSyncVariance/0",
+    "SendSyncVariance/1",
+    "SendSyncVariance/2",
+]
+
+HASHER = {
+    ## SendSyncVariance
+    "NaiveSendForSend": Rank.LOW,
+    "NaiveSyncForSync": Rank.LOW,
+    #
+    "ApiSyncforSync": Rank.MID,
+    "PhantomSendForSend": Rank.MID,
+    "RelaxSync": Rank.MID,
+    #
+    "ApiSendForSync": Rank.HIGH,
+    "RelaxSend": Rank.HIGH,
+    
+    ## UnsafeDataflow
+    "PtrAsRef": Rank.LOW,
+    "SliceFromRaw": Rank.LOW,
+    "SliceUnchecked": Rank.LOW,
+    "Transmute": Rank.LOW,
+    #
+    "CopyFlow": Rank.MID,
+    "ReadFlow": Rank.MID,
+    "WriteFlow": Rank.MID,
+    #
+    "VecFromRaw": Rank.HIGH,
+    "VecSetLen": Rank.HIGH,
+}
 
 SUBCATEGORY = {
     "SendSyncVariance": [
@@ -111,13 +152,12 @@ for log_file_name in os.listdir(log_dir):
             AnalyzerField.SPAN_SET: set(),
         }
 
-        if analyzer in SUBCATEGORY:
-            for subcategory in SUBCATEGORY[analyzer]:
-                cur_stat[subcategory] = {
-                    AnalyzerField.TIME: datetime.timedelta(),
-                    AnalyzerField.NUM_REPORTS: 0,
-                    AnalyzerField.SPAN_SET: set(),
-                }
+        for rank in RANK_CATEGORIES:
+            cur_stat[rank] = {
+                AnalyzerField.TIME: datetime.timedelta(),
+                AnalyzerField.NUM_REPORTS: 0,
+                AnalyzerField.SPAN_SET: set(),
+            }
 
     with open(os.path.join(log_dir, log_file_name)) as log_file:
         crate_name = log_file_name[4:]
@@ -208,9 +248,9 @@ for log_file_name in os.listdir(log_dir):
 
                         # Initialize metadata for analyzer subcategories
                         if analyzer in SUBCATEGORY:
-                            for subcategory in SUBCATEGORY[analyzer]:
+                            for rank in Rank:
                                 # Time elapsed for subcategory is not counted.
-                                target_stat[subcategory] = {
+                                target_stat[f'{analyzer}/{rank}'] = {
                                     AnalyzerField.TIME: datetime.timedelta(),
                                     AnalyzerField.NUM_REPORTS: 0,
                                     AnalyzerField.SPAN_SET: set()
@@ -227,14 +267,17 @@ for log_file_name in os.listdir(log_dir):
                                 report_file_str = report_file.read().replace("\t", "\\t").replace("\u001B", "\\u001B")
                                 report_dict = tomlkit.loads(report_file_str)
                             for report in report_dict["reports"]:
-                                analyzer = report['analyzer'].split(':')[0]
+                                tmp = report['analyzer'].split(':/')
+                                analyzer = tmp[0]
                                 target_stat[analyzer][AnalyzerField.NUM_REPORTS] += 1
                                 target_stat[analyzer][AnalyzerField.SPAN_SET].add(report["location"])
 
                                 if analyzer in SUBCATEGORY:
-                                    for subcategory in filter(lambda x: x in report["analyzer"], SUBCATEGORY[analyzer]):
-                                        target_stat[subcategory][AnalyzerField.NUM_REPORTS] += 1
-                                        target_stat[subcategory][AnalyzerField.SPAN_SET].add(report["location"])
+                                    for subcategory in tmp[1].split('/'):
+                                        span, cur_rank = report['location'], HASHER[subcategory]
+                                        for rank in Rank:
+                                            if cur_rank >= rank:
+                                                target_stat[f'{analyzer}/{rank}'][AnalyzerField.SPAN_SET].add(span)
 
                         # Accumulate target stat to crate stat
                         if crate_status == Status.OKAY:
@@ -249,12 +292,13 @@ for log_file_name in os.listdir(log_dir):
                                         else:
                                             cur_stat[analyzer][field] += target_stat[analyzer][field]
                                 if analyzer in SUBCATEGORY:
-                                    for subcategory in SUBCATEGORY[analyzer]:
+                                    for rank in Rank:
+                                        key = f'{analyzer}/{rank}'
                                         for field in AnalyzerField:
-                                            if isinstance(target_stat[subcategory][field], set):
-                                                cur_stat[subcategory][field] |= target_stat[subcategory][field]
+                                            if isinstance(target_stat[key][field], set):
+                                                cur_stat[key][field] |= target_stat[key][field]
                                             else:
-                                                cur_stat[subcategory][field] += target_stat[subcategory][field]
+                                                cur_stat[key][field] += target_stat[key][field]
                         target_stat = None
 
             prev_line = line
@@ -274,7 +318,7 @@ print(crate_stat["status_acc"])
 one_ms = datetime.timedelta(milliseconds=1)
 
 # CSV export of successful crates
-with open(f"stat-{sys.argv[1]}.csv", 'w', newline='') as csvfile:
+with open(f"stat-{sys.argv[1]}-rank.csv", 'w', newline='') as csvfile:
     csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
 
     header_row = ["name", "targets", "e2e-time", "rudra-time"]
@@ -284,10 +328,9 @@ with open(f"stat-{sys.argv[1]}.csv", 'w', newline='') as csvfile:
         header_row.append(f"{analyzer}-num-span")
 
         if analyzer in SUBCATEGORY:
-            for subcategory in SUBCATEGORY[analyzer]:
-                # header_row.append(f"{subcategory}-time")
-                header_row.append(f"{subcategory}-num-reports")
-                header_row.append(f"{subcategory}-num-span")
+            for rank in Rank:
+                key = f'{analyzer}/{rank}'
+                header_row.append(f"{key}-num-span")
 
     header_row.append(f"total-reports")
     header_row.append(f"total-span")
@@ -310,10 +353,9 @@ with open(f"stat-{sys.argv[1]}.csv", 'w', newline='') as csvfile:
                 crate_row.append(len(stat[analyzer][AnalyzerField.SPAN_SET]))
 
                 if analyzer in SUBCATEGORY:
-                    for subcategory in SUBCATEGORY[analyzer]:
-                        # crate_row.append(stat[subcategory][AnalyzerField.TIME] / one_ms)  # ms taken
-                        crate_row.append(stat[subcategory][AnalyzerField.NUM_REPORTS])
-                        crate_row.append(len(stat[subcategory][AnalyzerField.SPAN_SET]))
+                    for rank in Rank:
+                        key = f'{analyzer}/{rank}'
+                        crate_row.append(len(stat[key][AnalyzerField.SPAN_SET]))
 
                 total_reports += stat[analyzer][AnalyzerField.NUM_REPORTS]
                 total_span += len(stat[analyzer][AnalyzerField.SPAN_SET])
@@ -321,9 +363,225 @@ with open(f"stat-{sys.argv[1]}.csv", 'w', newline='') as csvfile:
             crate_row.append(total_span)
             csv_writer.writerow(crate_row)
 
-with open(f"status-{sys.argv[1]}.csv", 'w', newline='') as csvfile:
+with open(f"status-{sys.argv[1]}-rank.csv", 'w', newline='') as csvfile:
     csv_writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
 
     csv_writer.writerow(("name", "status"))
     for (i, name) in enumerate(crate_stat["names"]):
         csv_writer.writerow((name, crate_stat["status"][i]))
+
+tp_cnt = count_by_rank.count_reported_tp(
+    common.get_poc_detailed_metadata(),
+    f'{campaign_dir}/{experiment_ver}/report',
+)
+unreported_tp_cnt = count_by_rank.count_unreported_tp(
+    f'{campaign_dir}/{experiment_ver}/report',
+)
+
+stat = pd.read_csv(f'stat-{experiment_ver}-rank.csv')
+
+# Don't forget to take into account 3 additional bugs that are not represented as pocs in Rudra-PoC.
+stat['total-span'][0] += 3
+# https://github.com/rust-lang/rust/issues/80894 => VecSetLen (Rank.HIGH)
+# https://github.com/rust-lang/rust/issues/80335 => SliceUnchecked (Rank.LOW)
+# => Add 2 to Rank.LOW, Add 1 to Rank.MID, Add 1 to Rank.HIGH
+tp_cnt['UnsafeDataflow'][Rank.LOW] += 2
+tp_cnt['UnsafeDataflow'][Rank.MID] += 1
+tp_cnt['UnsafeDataflow'][Rank.HIGH] += 1
+stat['UnsafeDataflow-num-span'][0] += 2
+stat['UnsafeDataflow/2-num-span'][0] += 1
+stat['UnsafeDataflow/1-num-span'][0] += 1
+stat['UnsafeDataflow/0-num-span'][0] += 2
+# https://github.com/rust-lang/rust/issues/81425 => ApiSyncforSync/NaiveSyncForSync/RelaxSync (Rank.MID/LOW)
+# => Add 1 to Rank.MID, Add 1 to Rank.LOW
+tp_cnt['SendSyncVariance'][Rank.LOW] += 1
+tp_cnt['SendSyncVariance'][Rank.MID] += 1
+stat['SendSyncVariance-num-span'][0] += 1
+stat['SendSyncVariance/1-num-span'][0] += 1
+stat['SendSyncVariance/0-num-span'][0] += 1
+
+print("""
+\\newcommand{\\cratesioCrate}{\\num{%d}\\xspace}
+\\newcommand{\\cratesioCrateShort}{%dk\\xspace}
+
+\\newcommand{\\totalTimeHours}{%d hours\\xspace}
+
+\\newcommand{\\compileOkay}{\\num{%d}\\xspace}
+\\newcommand{\\compileError}{\\num{%d}\\xspace}
+\\newcommand{\\compileEmpty}{\\num{%d}\\xspace}
+\\newcommand{\\compileMetadataError}{\\num{%d}\\xspace}
+
+\\newcommand{\\elapsedTotalAverage}{\\num{%d}~sec\\xspace}
+\\newcommand{\\elapsedRudraAverage}{\\num{%d}~ms\\xspace}
+\\newcommand{\\elapsedSvAverage}{\\num{%d}~ms\\xspace}
+\\newcommand{\\elapsedUdAverage}{\\num{%d}~ms\\xspace}
+
+\\newcommand{\\reportsAll}{\\num{%d}\\xspace}
+
+\\newcommand{\\udReportsAll}{\\num{%d}\\xspace}
+\\newcommand{\\udReportsHigh}{\\num{%d}\\xspace}
+\\newcommand{\\udReportsMed}{\\num{%d}\\xspace}
+\\newcommand{\\udReportsLow}{\\num{%d}\\xspace}
+
+\\newcommand{\\udBugsAll}{\\num{%d}\\xspace}
+\\newcommand{\\udBugsHigh}{\\num{%d}\\xspace}
+\\newcommand{\\udBugsMed}{\\num{%d}\\xspace}
+\\newcommand{\\udBugsLow}{\\num{%d}\\xspace}
+
+\\newcommand{\\udBugsAllPercentage}{%d\\%%\\xspace}
+\\newcommand{\\udBugsHighPercentage}{%d\\%%\\xspace}
+\\newcommand{\\udBugsMedPercentage}{%d\\%%\\xspace}
+\\newcommand{\\udBugsLowPercentage}{%d\\%%\\xspace}
+
+\\newcommand{\\udInternalAll}{\\num{%d}\\xspace}
+\\newcommand{\\udInternalHigh}{\\num{%d}\\xspace}
+\\newcommand{\\udInternalMed}{\\num{%d}\\xspace}
+\\newcommand{\\udInternalLow}{\\num{%d}\\xspace}
+
+\\newcommand{\\udInternalAllPercentage}{%d\\%%\\xspace}
+\\newcommand{\\udInternalHighPercentage}{%d\\%%\\xspace}
+\\newcommand{\\udInternalMedPercentage}{%d\\%%\\xspace}
+\\newcommand{\\udInternalLowPercentage}{%d\\%%\\xspace}
+
+\\newcommand{\\udBugSumAll}{\\num{%d}\\xspace}
+\\newcommand{\\udBugSumHigh}{\\num{%d}\\xspace}
+\\newcommand{\\udBugSumMed}{\\num{%d}\\xspace}
+\\newcommand{\\udBugSumLow}{\\num{%d}\\xspace}
+
+\\newcommand{\\udBugSumAllPercentage}{%d\\%%\\xspace}
+\\newcommand{\\udBugSumHighPercentage}{%d\\%%\\xspace}
+\\newcommand{\\udBugSumMedPercentage}{%d\\%%\\xspace}
+\\newcommand{\\udBugSumLowPercentage}{%d\\%%\\xspace}
+
+\\newcommand{\\svReportsAll}{\\num{%d}\\xspace}
+\\newcommand{\\svReportsHigh}{\\num{%d}\\xspace}
+\\newcommand{\\svReportsMed}{\\num{%d}\\xspace}
+\\newcommand{\\svReportsLow}{\\num{%d}\\xspace}
+
+\\newcommand{\\svBugsAll}{\\num{%d}\\xspace}
+\\newcommand{\\svBugsHigh}{\\num{%d}\\xspace}
+\\newcommand{\\svBugsMed}{\\num{%d}\\xspace}
+\\newcommand{\\svBugsLow}{\\num{%d}\\xspace}
+
+\\newcommand{\\svBugsAllPercentage}{%d\\%%\\xspace}
+\\newcommand{\\svBugsHighPercentage}{%d\\%%\\xspace}
+\\newcommand{\\svBugsMedPercentage}{%d\\%%\\xspace}
+\\newcommand{\\svBugsLowPercentage}{%d\\%%\\xspace}
+
+\\newcommand{\\svInternalAll}{\\num{%d}\\xspace}
+\\newcommand{\\svInternalHigh}{\\num{%d}\\xspace}
+\\newcommand{\\svInternalMed}{\\num{%d}\\xspace}
+\\newcommand{\\svInternalLow}{\\num{%d}\\xspace}
+
+\\newcommand{\\svInternalAllPercentage}{%d\\%%\\xspace}
+\\newcommand{\\svInternalHighPercentage}{%d\\%%\\xspace}
+\\newcommand{\\svInternalMedPercentage}{%d\\%%\\xspace}
+\\newcommand{\\svInternalLowPercentage}{%d\\%%\\xspace}
+
+\\newcommand{\\svBugSumAll}{\\num{%d}\\xspace}
+\\newcommand{\\svBugSumHigh}{\\num{%d}\\xspace}
+\\newcommand{\\svBugSumMed}{\\num{%d}\\xspace}
+\\newcommand{\\svBugSumLow}{\\num{%d}\\xspace}
+
+\\newcommand{\\svBugSumAllPercentage}{%d\\%%\\xspace}
+\\newcommand{\\svBugSumHighPercentage}{%d\\%%\\xspace}
+\\newcommand{\\svBugSumMedPercentage}{%d\\%%\\xspace}
+\\newcommand{\\svBugSumLowPercentage}{%d\\%%\\xspace}
+""" % (
+    len(stat['name']), # Total Crates
+    len(stat['name']) // 1000, # 1000,
+
+    round(stat['e2e-time'].sum() / (3600000), 1), # milliseconds to hour conversion
+    
+    crate_stat['status_acc'][Status.OKAY], # Compile Okay
+    crate_stat['status_acc'][Status.EARLY_COMPILE_ERROR] + crate_stat['status_acc'][Status.TYPE_COMPILE_ERROR] + crate_stat['status_acc'][Status.LINT_COMPILE_ERROR] + crate_stat['status_acc'][Status.ONLY_MAC_OS_ERROR], # Compile Error
+    crate_stat['status_acc'][Status.EMPTY_TARGET], # Compile Empty
+    crate_stat['status_acc'][Status.METADATA_ERROR], # compileMetadataError
+
+    stat['e2e-time'].mean() / 1000, # elapsedTotalAverage (s)
+    stat['rudra-time'].mean(), # elapsedRudraAverage (ms)
+    stat['SendSyncVariance-time'].sum() / crate_stat['status_acc'][Status.OKAY], # elapsedSvAverage
+    stat['UnsafeDataflow-time'].sum() / crate_stat['status_acc'][Status.OKAY], # elapsedUdAverage
+
+    # Total Count (of reports)
+    stat['total-span'].sum(),
+
+    # UD Count
+    stat['UnsafeDataflow-num-span'].sum(), # udReportsAll
+    stat['UnsafeDataflow/2-num-span'].sum(),
+    stat['UnsafeDataflow/1-num-span'].sum(),
+    stat['UnsafeDataflow/0-num-span'].sum(),
+
+    tp_cnt['UnsafeDataflow'][Rank.LOW], # UDBugsAll
+    tp_cnt['UnsafeDataflow'][Rank.HIGH], # UDBugsHigh
+    tp_cnt['UnsafeDataflow'][Rank.MID], # UDBugsMed
+    tp_cnt['UnsafeDataflow'][Rank.LOW], # UDBugsLow
+    
+    round(tp_cnt['UnsafeDataflow'][Rank.LOW]/stat['UnsafeDataflow-num-span'].sum() * 100, 1), # udBugsAllPercentage
+    round(tp_cnt['UnsafeDataflow'][Rank.HIGH]/stat['UnsafeDataflow/2-num-span'].sum() * 100, 1), # udBugsHighPercentage
+    round(tp_cnt['UnsafeDataflow'][Rank.MID]/stat['UnsafeDataflow/1-num-span'].sum() * 100, 1), # udBugsMedPercentage
+    round(tp_cnt['UnsafeDataflow'][Rank.LOW]/stat['UnsafeDataflow/0-num-span'].sum() * 100, 1), # udBugsLowPercentage
+
+    unreported_tp_cnt['UnsafeDataflow'][Rank.LOW], # udInternalAll
+    unreported_tp_cnt['UnsafeDataflow'][Rank.HIGH], # udInternalHigh
+    unreported_tp_cnt['UnsafeDataflow'][Rank.MID], # udInternalMed
+    unreported_tp_cnt['UnsafeDataflow'][Rank.LOW], # udInternalLow
+
+    # udInternalAllPercentage
+    round(unreported_tp_cnt['UnsafeDataflow'][Rank.LOW]/stat['UnsafeDataflow-num-span'].sum() * 100, 1),
+    round(unreported_tp_cnt['UnsafeDataflow'][Rank.HIGH]/stat['UnsafeDataflow/2-num-span'].sum() * 100, 1),
+    round(unreported_tp_cnt['UnsafeDataflow'][Rank.MID]/stat['UnsafeDataflow/1-num-span'].sum() * 100, 1),
+    round(unreported_tp_cnt['UnsafeDataflow'][Rank.LOW]/stat['UnsafeDataflow/0-num-span'].sum() * 100, 1),
+
+    # udBugSumAll
+    tp_cnt['UnsafeDataflow'][Rank.LOW] + unreported_tp_cnt['UnsafeDataflow'][Rank.LOW],
+    tp_cnt['UnsafeDataflow'][Rank.HIGH] + unreported_tp_cnt['UnsafeDataflow'][Rank.HIGH],
+    tp_cnt['UnsafeDataflow'][Rank.MID] + unreported_tp_cnt['UnsafeDataflow'][Rank.MID],
+    tp_cnt['UnsafeDataflow'][Rank.LOW] + unreported_tp_cnt['UnsafeDataflow'][Rank.LOW],
+
+    # udBugSumAllPercentage
+    round((tp_cnt['UnsafeDataflow'][Rank.LOW] + unreported_tp_cnt['UnsafeDataflow'][Rank.LOW]) / stat['UnsafeDataflow-num-span'].sum() * 100, 1),
+    round((tp_cnt['UnsafeDataflow'][Rank.HIGH] + unreported_tp_cnt['UnsafeDataflow'][Rank.HIGH]) / stat['UnsafeDataflow/2-num-span'].sum() * 100, 1),
+    round((tp_cnt['UnsafeDataflow'][Rank.MID] + unreported_tp_cnt['UnsafeDataflow'][Rank.MID]) / stat['UnsafeDataflow/1-num-span'].sum() * 100, 1),
+    round((tp_cnt['UnsafeDataflow'][Rank.LOW] + unreported_tp_cnt['UnsafeDataflow'][Rank.HIGH]) / stat['UnsafeDataflow/0-num-span'].sum() * 100, 1),
+
+    # SV Count
+    stat['SendSyncVariance-num-span'].sum(),
+    stat['SendSyncVariance/2-num-span'].sum(),
+    stat['SendSyncVariance/1-num-span'].sum(),
+    stat['SendSyncVariance/0-num-span'].sum(),
+    
+    tp_cnt['SendSyncVariance'][Rank.LOW], # SVBugsAll
+    tp_cnt['SendSyncVariance'][Rank.HIGH], # SVBugsHigh
+    tp_cnt['SendSyncVariance'][Rank.MID], # SVBugsMed
+    tp_cnt['SendSyncVariance'][Rank.LOW], # SVBugsLow
+
+    round(tp_cnt['SendSyncVariance'][Rank.LOW]/stat['SendSyncVariance-num-span'].sum() * 100, 1), # svBugsAllPercentage
+    round(tp_cnt['SendSyncVariance'][Rank.HIGH]/stat['SendSyncVariance/2-num-span'].sum() * 100, 1), # svBugsHighPercentage
+    round(tp_cnt['SendSyncVariance'][Rank.MID]/stat['SendSyncVariance/1-num-span'].sum() * 100, 1), # svBugsMedPercentage
+    round(tp_cnt['SendSyncVariance'][Rank.LOW]/stat['SendSyncVariance/0-num-span'].sum() * 100, 1), # svBugsLowPercentage
+
+    # svInternalAll
+    unreported_tp_cnt['SendSyncVariance'][Rank.LOW], # svInternalAll
+    unreported_tp_cnt['SendSyncVariance'][Rank.HIGH], # svInternalHigh
+    unreported_tp_cnt['SendSyncVariance'][Rank.MID], # svInternalMed
+    unreported_tp_cnt['SendSyncVariance'][Rank.LOW], # svInternalLow
+
+    # svInternalAllPercentage
+    round(unreported_tp_cnt['SendSyncVariance'][Rank.LOW]/stat['SendSyncVariance-num-span'].sum() * 100, 1),
+    round(unreported_tp_cnt['SendSyncVariance'][Rank.HIGH]/stat['SendSyncVariance/2-num-span'].sum() * 100, 1),
+    round(unreported_tp_cnt['SendSyncVariance'][Rank.MID]/stat['SendSyncVariance/1-num-span'].sum() * 100, 1),
+    round(unreported_tp_cnt['SendSyncVariance'][Rank.LOW]/stat['SendSyncVariance/0-num-span'].sum() * 100, 1),
+
+    # svBugSumAll
+    tp_cnt['SendSyncVariance'][Rank.LOW] + unreported_tp_cnt['SendSyncVariance'][Rank.LOW],
+    tp_cnt['SendSyncVariance'][Rank.HIGH] + unreported_tp_cnt['SendSyncVariance'][Rank.HIGH],
+    tp_cnt['SendSyncVariance'][Rank.MID] + unreported_tp_cnt['SendSyncVariance'][Rank.MID],
+    tp_cnt['SendSyncVariance'][Rank.LOW] + unreported_tp_cnt['SendSyncVariance'][Rank.LOW],
+
+    # svBugSumAllPercentage
+    round((tp_cnt['SendSyncVariance'][Rank.LOW] + unreported_tp_cnt['SendSyncVariance'][Rank.LOW]) / stat['SendSyncVariance-num-span'].sum() * 100, 1),
+    round((tp_cnt['SendSyncVariance'][Rank.HIGH] + unreported_tp_cnt['SendSyncVariance'][Rank.HIGH]) / stat['SendSyncVariance/2-num-span'].sum() * 100, 1),
+    round((tp_cnt['SendSyncVariance'][Rank.MID] + unreported_tp_cnt['SendSyncVariance'][Rank.MID]) / stat['SendSyncVariance/1-num-span'].sum() * 100, 1),
+    round((tp_cnt['SendSyncVariance'][Rank.LOW] + unreported_tp_cnt['SendSyncVariance'][Rank.LOW]) / stat['SendSyncVariance/0-num-span'].sum() * 100, 1),
+))
